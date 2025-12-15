770300596505d2e57814102eb4f566e72bf93146
Nick Efford <nick.efford@gmail.com>
2025-12-15 14:07:18 +0000

    testing: added library version catalog

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -8,13 +8,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-config-yaml")
-    implementation("ch.qos.logback:logback-classic:1.5.13")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
-    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    testImplementation(libs.ktor.server.testHost)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotest.runner.junit5)
+    runtimeOnly(libs.ktor.server.configYaml)
+    runtimeOnly(libs.logback.classic)
 }
 
 kotlin {
